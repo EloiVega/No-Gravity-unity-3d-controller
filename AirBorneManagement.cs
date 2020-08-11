aaed5a2d48bb00d254c32e76ba825f75d0706cc3
EloiVega <50173172+EloiVega@users.noreply.github.com>
2020-08-11 04:58:44 +0200

    Movement and Jumping
    Movement controls character leaning sideways and velocity control and AirborneManagement uses ray casting to detect ground and jump if there is ground beneath

---
+++ AirBorneManagement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class AirBorneManagement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float JumpHeight;
+
+    public enum State { OnGround, InAir };
+    public static State state = State.OnGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray Down = new Ray(transform.position, -Vector3.up);
+        RaycastHit HitInfo;
+
+        if (Physics.Raycast(Down, out HitInfo, 1f))
+        {
+            Debug.DrawLine(Down.origin, HitInfo.point, Color.green);
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            state = State.OnGround;
+        }
+        else if(Physics.Raycast(Down, out HitInfo, 1.5f)){
+            Debug.DrawLine(Down.origin, HitInfo.point, Color.yellow);
+            //rb.velocity = new Vector3(rb.velocity.x/2, rb.velocity.y, rb.velocity.z/2);
+            state = State.InAir;
+        }
+        else
+        {
+            Debug.DrawLine(Down.origin, Down.origin + Down.direction * 1.5f, Color.red);
+            rb.AddForce(0, -JumpHeight/5 * Time.deltaTime, 0);
+            state = State.InAir;
+        }
+
+        if(Physics.Raycast(Down, out HitInfo, 1.5f))if (Input.GetKey(KeyCode.Space)) { rb.AddForce(0,  JumpHeight * Time.deltaTime, 0); }
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float rotationspeed;
+
+    float rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            rotation -= speed * Time.deltaTime;
+            rotation = Mathf.Clamp(rotation, -37, 37);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 0f, rotation), speed * Time.deltaTime);
+
+
+            if (AirBorneManagement.state == AirBorneManagement.State.OnGround) GetComponent<Rigidbody>().AddForce((speed / 4) * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rotation += speed * Time.deltaTime;
+            rotation = Mathf.Clamp(rotation, -37, 37);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 0f, rotation), speed * Time.deltaTime);
+
+            if (AirBorneManagement.state == AirBorneManagement.State.OnGround) GetComponent<Rigidbody>().AddForce((-speed / 4) * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 0f, 0f), rotationspeed * Time.deltaTime);
+            rotation = 0f;
+        }
+    }
+}
